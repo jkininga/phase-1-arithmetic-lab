ce8e4cc88074cfa3b2ac652e430ed606b52eb85e
Caroline Ininga <carolineininga@gmail.com>
2025-03-21 11:46:13 +0300

    completed arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Write your code here
+
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2 
+
+
+let random = Math.floor((Math.random() * 10 + 1))
+
+let num3 = 9
+let num4 = 5
+
+let mod = num3 % num4
+
+let array = [10,13,16, 19, 20,11]
+
+let max = Math.max(10,13,16, 19, 20,11)
+
+console.log(max)
+
+//console.log((Math.random() * 10 + 1))
+
+
